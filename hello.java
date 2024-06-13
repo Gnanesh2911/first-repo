a8a22655598c0042ed98de50a2b3f4e43deb4694
Gnanesh2911 <gnaneshkumar.jeyakumar@gmail.com>
2024-06-13 14:51:42 +0530

    multiplied

--- hello.java
+++ hello.java
@@ -4,7 +4,7 @@ public class hello
     System.out.println("hello");
     int a = 5;
     int c = 20;
-    int b = a+c;
+    int b = a*c;
     System.out.println(b);
     System.out.println("my name is gnanesh");
     int gna = 25;
