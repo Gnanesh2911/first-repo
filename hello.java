cdcadd75115615245cd08c89783c85cbfe3280c3
Gnanesh2911 <gnaneshkumar.jeyakumar@gmail.com>
2024-06-13 14:21:26 +0530

    added

--- hello.java
+++ hello.java
@@ -2,5 +2,9 @@ public class hello
 {
   public static void main(String[] args) {
     System.out.println("hello");
+    int a = 5;
+    int c = 20;
+    int b = a+c;
+    System.out.println(b);
   }
 }
